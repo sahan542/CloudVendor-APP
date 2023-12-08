87942b0a03b1f5242d57fb625f68b71d5d9f93aa
sahan542 <sahanrashmika542@gmail.com>
2023-12-08 16:10:59 +0530

    Create & Post requests

---
+++ src/main/java/com/example/restvendor/controller/CloudVendorAPIService.java
@@ -0,0 +1,22 @@
+package com.example.restvendor.controller;
+
+
+import com.example.restvendor.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService {
+
+    CloudVendor cloudVendor;
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return cloudVendor;
+    }
+
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor created Successfully";
+    }
+}
