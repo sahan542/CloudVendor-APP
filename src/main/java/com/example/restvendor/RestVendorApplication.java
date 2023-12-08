c34f50fc5bbf6aefed35507fae19cf81fa2e21d5
sahan542 <sahanrashmika542@gmail.com>
2023-12-08 14:14:22 +0530

    project start & springboot setup

---
+++ src/main/java/com/example/restvendor/RestVendorApplication.java
@@ -0,0 +1,13 @@
+package com.example.restvendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestVendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restvendor/RestVendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restvendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
